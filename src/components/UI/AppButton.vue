6608d547a6f78530649d2b18c418072097c61a14
Astashin Oleg <astashinoleg@gmail.com>
2023-04-18 17:06:23 +0300

    -- first UI components

---
+++ src/components/UI/AppButton.vue
@@ -0,0 +1,50 @@
+<template>
+    <a :href="activeUrl" class="btn-box" :class="btnColor">
+        <slot></slot>
+    </a>
+</template>
+
+<script>
+
+export default {
+  props: {
+    activeUrl: String,
+    btnColor: String
+  }
+}
+</script>
+
+<style scoped>
+.btn-box {
+    width: 200px;
+    height: 30px;
+    /* background-color: #baaca1; */
+    position: relative;
+    display: flex;
+    justify-content: center;
+    text-align: center;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    font-size: 20px;
+
+    text-decoration: none;
+    text-transform: uppercase;
+    letter-spacing: 0.3px;
+    cursor: pointer;
+}
+
+.btn-dark {
+    background-color: #3d4549;
+    color: #fff;
+
+}
+
+.btn-light {
+    background-color: #baaca1;
+    color: #3d4549;
+}
+
+.btn-box:hover {
+    color: #ffffff;
+}
+</style>
---
+++ src/components/UI/AppMenuList.vue
@@ -0,0 +1,75 @@
+<template>
+    <nav :class="[wrap, widthFull]">
+        <ul v-for="elem in menu" :key="elem.title" >
+            <li :class="[color]"><a :href="elem.url" :class="[color]">{{ elem.title}}</a></li>
+        </ul>
+    </nav>
+</template>
+
+<script>
+
+export default {
+  props: {
+    menu: Array,
+    wrap: String,
+    width: String,
+    color: String
+  }
+//   data () {
+//     return {
+//         wrapClass: 'yes'
+//     }
+//   }
+}
+</script>
+
+<style scoped>
+nav {
+    display: inline-flex;
+    justify-content: space-between;
+    gap: 26px;
+    row-gap: 0px;
+}
+
+ul {
+    padding: 0;
+    width: fit-content;
+    height: 10px;
+
+}
+
+a, li {
+    list-style: none;
+    text-decoration: none;
+    font-size: 20px;
+    font-weight: 300;
+}
+
+a:hover {
+    text-decoration: underline 1px;
+}
+
+.wrap {
+    flex-wrap: wrap;
+}
+
+.nowrap {
+    flex-wrap: nowrap;
+}
+
+.widthFull {
+    width: 100%;
+}
+
+.width-50 {
+    width: 50%;
+}
+
+.colorDark {
+    color: #3d4549;
+}
+
+.colorLight {
+    color: #ffffff;
+}
+</style>
